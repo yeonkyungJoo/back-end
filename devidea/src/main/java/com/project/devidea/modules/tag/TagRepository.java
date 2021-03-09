d5de9148de16097ffa0f2b6df5c686a630f6d14b
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2021-03-09 15:20:31 +0900

    태그,존 추가

---
+++ devidea/src/main/java/com/project/devidea/modules/tag/TagRepository.java
@@ -0,0 +1,14 @@
+package com.project.devidea.modules.tag;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Transactional
+public interface TagRepository extends JpaRepository<Tag,Long> {
+    @EntityGraph(attributePaths = {"parent", "children"}, type = EntityGraph.EntityGraphType.LOAD)
+    List<Tag> findAll();
+    @EntityGraph(attributePaths = {"parent", "children"}, type = EntityGraph.EntityGraphType.LOAD)
+    Tag findByFirstName(String firstname);
+}
--- devidea/src/main/java/com/project/devidea/modules/zone/ZoneRepository.java
+++ devidea/src/main/java/com/project/devidea/modules/zone/ZoneRepository.java
@@ -1,8 +1,11 @@
 package com.project.devidea.modules.zone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
+    List<Zone> findAll();
     Zone findByCityAndProvince(String cityName, String provinceName);
 }
 
