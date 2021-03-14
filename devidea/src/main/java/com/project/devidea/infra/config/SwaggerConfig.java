8b6fe24ba792edf8873d4bf02ef0b350c0cdcb73
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2021-03-14 22:12:10 +0900

    1.살짝 변경 2. swagger 의존성 및 config추가!

--- devidea/src/main/java/com/project/devidea/infra/config/SwaggerConfig.java
+++ devidea/src/main/java/com/project/devidea/infra/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors
-                        .basePackage("com.project.devidea.modules.content.study"))
+                        .basePackage("com.project.devidea"))
                 .paths(PathSelectors.any()).build()
                 .apiInfo(apiInfo());
     }
