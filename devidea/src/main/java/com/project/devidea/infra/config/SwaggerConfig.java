90e912b059365730540cce87f8e004898a114e3f
hwangsero <hsro2228@naver.com>
2021-03-15 22:24:37 +0900

    CI 테스트1

--- devidea/src/main/java/com/project/devidea/infra/config/SwaggerConfig.java
+++ devidea/src/main/java/com/project/devidea/infra/config/SwaggerConfig.java
@@ -15,6 +15,7 @@ import java.util.Collections;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
