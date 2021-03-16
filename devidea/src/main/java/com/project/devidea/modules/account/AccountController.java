a93955f3b4e6687773d62e1eb05a10ead7a2492c
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-03-16 16:35:43 +0900

    Feat : 에러처리를 위한 RestControllerAdvice 및 테스트(통과완료)

--- devidea/src/main/java/com/project/devidea/modules/account/AccountController.java
+++ devidea/src/main/java/com/project/devidea/modules/account/AccountController.java
@@ -1,22 +1,21 @@
 package com.project.devidea.modules.account;
 
-import com.project.devidea.infra.config.jwt.JwtTokenUtil;
 import com.project.devidea.infra.config.oauth.OAuthService;
 import com.project.devidea.infra.config.oauth.provider.SocialLoginType;
+import com.project.devidea.modules.account.exception.SignUpRequestNotValidException;
 import com.project.devidea.modules.account.form.LoginRequestDto;
 import com.project.devidea.modules.account.form.SignUpRequestDto;
+import com.project.devidea.modules.account.validator.SignUpRequestValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
+import javax.validation.Valid;
 import java.util.Map;
 
 @RestController
@@ -26,10 +25,19 @@ public class AccountController {
 
     private final AccountService accountService;
     private final OAuthService oAuthService;
-    private final JwtTokenUtil jwtTokenUtil;
+    private final SignUpRequestValidator signUpRequestValidator;
+
+    @InitBinder("signUpRequestDto")
+    public void initSignUpRequestDtoValidator(WebDataBinder binder) {
+        binder.addValidators(signUpRequestValidator);
+    }
 
     @PostMapping("/sign-up")
-    public ResponseEntity<?> signUp(@RequestBody SignUpRequestDto signUpRequestDto) throws Exception {
+    public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequestDto signUpRequestDto, Errors errors) throws Exception {
+        if (errors.hasErrors()) {
+            throw new SignUpRequestNotValidException("회원가입 폼의 입력값을 확인해주세요.", errors);
+        }
+
         return new ResponseEntity<>(accountService.save(signUpRequestDto), HttpStatus.OK);
     }
 
@@ -59,12 +67,4 @@ public class AccountController {
         return new ResponseEntity<>(headers, HttpStatus.OK);
     }
 
-    @PostMapping("/asdf")
-    public void loginConfirm(HttpServletRequest request) {
-        String token = request.getHeader("Authorization");
-        String email = jwtTokenUtil.getUsernameFromToken(token.substring(7));
-        System.out.println();
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        System.out.println(authentication.getAuthorities().stream().);
-    }
 }
---
+++ devidea/src/main/java/com/project/devidea/modules/account/AccountControllerAdvice.java
@@ -0,0 +1,48 @@
+package com.project.devidea.modules.account;
+
+import com.project.devidea.modules.account.exception.ErrorFieldAndMessage;
+import com.project.devidea.modules.account.exception.AccountErrorMessage;
+import com.project.devidea.modules.account.exception.SignUpRequestNotValidException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice(basePackages = "com.project.devidea.modules.account")
+public class AccountControllerAdvice {
+
+    @ExceptionHandler(SignUpRequestNotValidException.class)
+    public ResponseEntity<AccountErrorMessage> signUp(SignUpRequestNotValidException signUpRequestNotValidException) {
+
+        AccountErrorMessage errorMessage = AccountErrorMessage.builder()
+                .time(LocalDateTime.now())
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .errorFieldAndMessageList(signUpRequestNotValidException.getErrors().getFieldErrors()
+                        .stream()
+                        .map(fieldError -> ErrorFieldAndMessage.builder()
+                                .field(fieldError.getField())
+                                .message(fieldError.getDefaultMessage())
+                                .build())
+                        .collect(Collectors.toList()))
+                .description(signUpRequestNotValidException.getMessage())
+                .build();
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<AccountErrorMessage> login(BadCredentialsException badCredentialsException) {
+
+        AccountErrorMessage errorMessage = AccountErrorMessage.builder()
+                .time(LocalDateTime.now())
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .description("회원의 아이디와 비밀번호가 일치하지 않습니다.")
+                .build();
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+}
