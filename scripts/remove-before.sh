3fc851f9213698888878809fe9767dab02919a18
hwangsero <hsro2228@naver.com>
2021-03-18 16:32:32 +0900

    CI 테스트23

--- scripts/remove-before.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find /opt/codedeploy-agent/deployment-root/c414d20e-1274-4a04-bad2-5a03565477ec/* -maxdepth 0 -type 'd' | grep -v
-$(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/c414d20e-1274-4a04-bad2-5a03565477ec/* | sort -t: -n |
-tail -1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
